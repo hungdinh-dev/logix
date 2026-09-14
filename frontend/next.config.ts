b5a6819d53cebf1a36ab2553665aa4ad9d20600c
HungDev <baohungdinhnguyen1412@gmail.com>
2026-09-14 14:22:46 +0700

    [Config]: Base FE Configs & Styles

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [100, 75],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
